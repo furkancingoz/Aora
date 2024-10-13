f52c645d64f8fb8eb042b605a5ca92eb28d6de32
Furkan Cingöz <furkancingoz@Furkan-MacBook-Air.local>
2024-10-13 17:29:07 +0300

    Add Sign In and Sign Up Screens for Authentication
    - Implemented Sign In screen with input fields for email and password.
    - Added Sign Up screen for new user registration.
    - Integrated both screens with consistent styling and layout.
    - Ensured responsiveness and compatibility with different screen sizes.

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { ScrollView, View, Text, Image } from 'react-native'
+import React,  { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router';
+
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full flex justify-center min-h-[60vh] px-4">
+          <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]" />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Login in to Aora</Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-adress"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(p) => setForm({ ...form, password: p })}
+            otherStyles="mt-7"
+          />
+        </View>
+        <CustomButton 
+          title="Sign In"
+          handlePress={submit}
+          containerStyles="mt-0"
+          isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Dont't have a account?
+            </Text>
+            <Link href="/sign-up" className="text-lg font-psemibold text-secondary">
+            Sign Up
+            </Link>
+          </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
